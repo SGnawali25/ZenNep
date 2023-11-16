b471acf4303f7a7ed7187aba9975fd9f4b24fac9
Sandesh Gnawali <sandeshgnawali@Sandeshs-MacBook-Air.local>
2023-11-16 02:20:01 -0600

    main.jsx change

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -20,8 +20,9 @@ function App() {
   return (
     <div>
       <Router>
+        <Header/>
           <Routes>
-          <Route path='/' Component={Login} exact />
+          <Route path='/' Component={Signin} exact />
           </Routes>
       </Router>
     </div>
