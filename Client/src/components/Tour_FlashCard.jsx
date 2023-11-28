0f5cfac429056189e68bc80516d3a81fd4c3e11d
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-11-28 09:09:41 +0545

    Search Button Fix 2

--- Client/src/components/Tour_FlashCard.jsx
+++ Client/src/components/Tour_FlashCard.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Tour_FlashCard({place}) {
+function Tour_FlashCard({ place }) {
   return (
     <div>
       <div className="blog-card">
         <div className="card-img">
-          <Link to={`/place/${place._id}`}><img src={place.images[0].url} /></Link>
+          <Link to={`/place/${place._id}`}>
+            <img src={place.images[0].url} />
+          </Link>
           <h1 className="PlaceName">{place.name}</h1>
         </div>
-        <div className="card-details"></div>
+        <div className="card-details">place location</div>
         <div className="card-text">
-          <p>
-            {place.description}
-          </p>
+          <p>{place.description}</p>
         </div>
-        <Link to={`/place/${place._id}`}><div className="read-more">Read More</div></Link>
+        <Link to={`/place/${place._id}`}>
+          <div className="read-more">Read More</div>
+        </Link>
       </div>
     </div>
   );
