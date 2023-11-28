c15ed3b02dbecd236a1d2af85909a078720a71ee
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-11-28 13:27:02 +0545

    CSS for password change

--- Client/src/pages/changePassword.jsx
+++ Client/src/pages/changePassword.jsx
@@ -1,95 +1,92 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { Fragment, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { useAlert } from 'react-alert'
-import { useDispatch, useSelector } from 'react-redux'
-import { changePassword, clearErrors } from "../actions/userActions"
-import Header from '../components/Header'
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import { changePassword, clearErrors } from "../actions/userActions";
+import Header from "../components/Header";
 
 const UpdatePassword = () => {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
 
-    const [currentPassword, setCurrentPassword] = useState('')
-    const [newPassword, setNewPassword] = useState('')
-    const [confirmNewPassword, setConfirmNewPassword] = useState('')
+  const navigate = useNavigate();
+  const alert = useAlert();
+  const dispatch = useDispatch();
 
-    const navigate = useNavigate();
-    const alert = useAlert();
-    const dispatch = useDispatch();
+  const { error, loading, success } = useSelector(
+    (state) => state.changePassword
+  );
 
-    const { error, loading, success } = useSelector(state => state.changePassword)
-
-    useEffect(() => {
-
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors());
-        }
-
-        if (success) {
-            alert.success('Password updated successfully')
-            navigate('/me')
-        }
-
-    }, [dispatch, alert, error, success])
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-
-        const formData = new FormData();
-        formData.set('currentPassword', currentPassword);
-        formData.set('newPassword', newPassword);
-        formData.set('confirmNewPassword', confirmNewPassword);
-
-        dispatch(changePassword(formData))
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
     }
 
-    return (
-        <Fragment>
-            <Header/>
-            <div className="row wrapper">
-                <div className="col-10 col-lg-5">
-                    <form className="shadow-lg" onSubmit={submitHandler}>
-                        <h1 className="mt-2 mb-5">Update Password</h1>
-                        <div className="form-group">
-                            <label htmlFor="old_password_field">Current Password</label>
-                            <input
-                                type="password"
-                                id="old_password_field"
-                                className="form-control"
-                                value={currentPassword}
-                                onChange={(e) => setCurrentPassword(e.target.value)}
-                            />
-                        </div>
+    if (success) {
+      alert.success("Password updated successfully");
+      navigate("/me");
+    }
+  }, [dispatch, alert, error, success]);
 
-                        <div className="form-group">
-                            <label htmlFor="new_password_field">New Password</label>
-                            <input
-                                type="password"
-                                id="new_password_field"
-                                className="form-control"
-                                value={newPassword}
-                                onChange={(e) => setNewPassword(e.target.value)}
-                            />
-                        </div>
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-                        <div className="form-group">
-                            <label htmlFor="new_password_field">Confirm New Password</label>
-                            <input
-                                type="password"
-                                id="confirm_new_password_field"
-                                className="form-control"
-                                value={confirmNewPassword}
-                                onChange={(e) => setConfirmNewPassword(e.target.value)}
-                            />
-                        </div>
+    const formData = new FormData();
+    formData.set("currentPassword", currentPassword);
+    formData.set("newPassword", newPassword);
+    formData.set("confirmNewPassword", confirmNewPassword);
 
-                        <input type="submit" className="btn update-btn btn-block mt-4 mb-3" value="Change Password" disabled={loading ? true : false} />
-                    </form>
-                </div>
-            </div>
+    dispatch(changePassword(formData));
+  };
 
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <Header />
+      <div className="Contact_Container">
+        <div className="Contact_Content">
+          <form onSubmit={submitHandler}>
+            <h1>Update Password</h1>
+            <h3 htmlFor="old_password_field">Current Password</h3>
+            <input
+              type="password"
+              id="old_password_field"
+              className="form-control"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+            />
 
-export default UpdatePassword
+            <h3 htmlFor="new_password_field">New Password</h3>
+            <input
+              type="password"
+              id="new_password_field"
+              className="form-control"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+
+            <h3 htmlFor="new_password_field">Confirm New Password</h3>
+            <input
+              type="password"
+              id="confirm_new_password_field"
+              className="form-control"
+              value={confirmNewPassword}
+              onChange={(e) => setConfirmNewPassword(e.target.value)}
+            />
+
+            <input
+              type="submit"
+              className="btn update-btn btn-block mt-4 mb-3"
+              value="Change Password"
+              disabled={loading ? true : false}
+            />
+          </form>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default UpdatePassword;
--- Client/src/pages/userProfile.jsx
+++ Client/src/pages/userProfile.jsx
@@ -1,50 +1,58 @@
-import React, { Fragment } from 'react'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import Loader from "../components/Loader";
-import Header from '../components/Header';
+import Header from "../components/Header";
 
 const Profile = () => {
+  const { user, loading } = useSelector((state) => state.auth);
 
-    const { user, loading } = useSelector(state => state.auth)
-
-    return (
+  return (
+    <Fragment>
+      <Header />
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-            <Header/>
-            {loading ? <Loader /> : (
-                <Fragment>
-                    <h2 className="mt-5 ml-5">My Profile</h2>
-                    <div className="row justify-content-around mt-5 user-info">
-                        <div className="col-12 col-md-3">
-                            <figure className='avatar avatar-profile'>
-                                <img className="rounded-circle img-fluid" src={user.image.url} alt={user.name} />
-                            </figure>
-                            <Link to="/me/update" id="edit_profile" className="btn btn-primary btn-block my-5">
-                                Edit Profile
-                            </Link>
-                        </div>
+          <div className="Profile_box">
+            <div className="Profile_Content">
+              <h2 className="mt-5 ml-5">My Profile</h2>
+              <div className="row justify-content-around mt-5 user-info">
+                <div className="col-12 col-md-3">
+                  <figure className="avatar avatar-profile">
+                    <img
+                      className="rounded-circle img-fluid"
+                      src={user.image.url}
+                      alt={user.name}
+                    />
+                  </figure>
+                </div>
 
-                        <div className="col-12 col-md-5">
-                            <h4>Full Name</h4>
-                            <p>{user.name}</p>
+                <div className="col-12 col-md-5">
+                  <h4>Full Name</h4>
+                  <p>{user.name}</p>
 
-                            <h4>Email Address</h4>
-                            <p>{user.email}</p>
+                  <h4>Email Address</h4>
+                  <p>{user.email}</p>
 
-                            <h4>Joined On</h4>
-                            <p>{String(user.createdAt).substring(0, 10)}</p>
+                  <h4>Joined On</h4>
+                  <p>{String(user.createdAt).substring(0, 10)}</p>
 
-
-                            <Link to="/password/change" className="btn btn-primary btn-block mt-3">
-                                Change Password
-                            </Link>
-                        </div>
-                    </div>
-                </Fragment>
-            )}
+                  <Link
+                    to="/password/change"
+                    className="btn btn-primary btn-block mt-3"
+                  >
+                    Change Password
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
         </Fragment>
-    )
-}
+      )}
+    </Fragment>
+  );
+};
 
-export default Profile
+export default Profile;
